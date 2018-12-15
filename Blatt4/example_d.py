b58cb8ff36f567bd43d8ab2a8aa82abda531b18c
Thomas Lettau <thomas@shadowice.org>
2018-12-15 22:10:02 +0100

    Blatt4 hinzugefügt

---
+++ Blatt4/example_d.py
@@ -0,0 +1,22 @@
+from nonlinear import NonLinear
+from animator import Animator
+import numpy as np
+
+RG = NonLinear()
+
+RG.dt = 0.01
+RG.times = np.arange(-50, 50, RG.dt)
+
+RG.dz = 0.0001
+RG.z = np.arange(0, 1, RG.dz)
+
+# RG.T0 = 7
+# RG.a0 = np.exp(-RG.times**2 / RG.T0**2)
+size = len(RG.times)
+RG.a0 = np.zeros(size)
+RG.a0[np.where(abs(RG.times) < .5)] = 2.5 * np.pi
+
+RG.integrate()
+
+anim = Animator(RG)
+anim.start_animation()
